2dd1b16d44648ec29f28112c8887f4fccdd08f5e
Bruce C. Dillahunty <bdillahu@peachbush.com>
2019-06-11 15:16:44 -0400

    We have file list and document download working, with basic "pretty printing" of the results.

--- dynalist_download.py
+++ dynalist_download.py
@@ -18,24 +18,49 @@ Dynalist_add_url  = "https://dynalist.io/api/v1/inbox/add"
 body = {'token': bdillahuToken}  
 
 
+def parse_doc_list(raw_doc_list):
+    logger.info("called parse_doc_list")
+
+    doc_list = json.loads(raw_doc_list)
+    logger.info(f"Parsed doc_list: {doc_list}")
+    return doc_list
 
 
 def get_data(args):
     logger.info("called get_data")
-    
-    if args.list:
+
+    def list_docs():
+        logger.info("called list_docs")
+
         payload = {'token':bdillahuToken}
         logger.debug(f"POST: {Dynalist_list_url} {payload}")
         r = requests.post(Dynalist_list_url, json = payload)
         logger.debug("RESPONSE: " + r.text)
-        print(r.text)
+        return r
+
+    def file_content(file_id):
+        logger.info("called file_content")
+        payload = {'token':bdillahuToken, 'file_id':file_id}
+        logger.debug(f"POST: {Dynalist_read_url} {payload}")
+        r = requests.post(Dynalist_read_url, json = payload)
+        logger.debug("RESPONSE: " + r.text)
+        return r
+        
+    if args.list:
+        raw_doc_list = list_docs()
+        doc_list = json.loads(raw_doc_list.text)
+        print(json.dumps(doc_list, indent=4))
     if args.file:
         for file_id in args.file:
-            payload = {'token':bdillahuToken, 'file_id':file_id}
-            logger.debug(f"POST: {Dynalist_read_url} {payload}")
-            r = requests.post(Dynalist_read_url, json = payload)
-            logger.debug("RESPONSE: " + r.text)
-            print(r.text)
+            raw_file_contents = file_content(file_id)
+            file_contents = json.loads(raw_file_contents.text)
+            print(json.dumps(file_contents, indent=4))
+    if args.dump_all:
+        doc_list = list_docs()
+        # somehow parse that
+        for file_id in doc_list_parsed:
+            file_contents = file_content(file_id)
+            print(file_contents.text)
 
 
 
@@ -54,6 +79,9 @@ def get_parser():
     parser.add_argument("--file",
                         help="Download contents of file - can appear multiple times",
                         action="append")
+    parser.add_argument("--dump_all", 
+                        help="Dump contents of all documents",
+                        action="store_true")
     parser.set_defaults(func=get_data)
     subparsers = parser.add_subparsers(help="sub-command help")
 
